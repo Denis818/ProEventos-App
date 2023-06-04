b347b9e3f963e891e495c53209b6564b7abccc8b
Denis Developer <denispereiradosreis@gmail.com>
2023-06-04 16:42:25 -0300

    crianto tela de eventos

--- src/app/eventos/eventos.component.ts
+++ src/app/eventos/eventos.component.ts
@@ -6,8 +6,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './eventos.component.html',
   styleUrls: ['./eventos.component.scss'],
 })
+
 export class EventosComponent implements OnInit {
-  public eventos: any;
+  public larguraImagem: number = 150;
+  public margemImagem: number = 2;
+  public exibirImagem: boolean = true;
+
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  private _filtroLista: string = '';
 
   constructor(private http: HttpClient) { }
 
@@ -15,10 +22,33 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  public get filtroLista() {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+
+    return this.eventos.filter((evento: any) => 
+      evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1)
+  }
+
+  alterarImagem() {
+    this.exibirImagem = !this.exibirImagem;
+  }
+
   public getEventos(): void {
     this.http.get('https://localhost:7186/api/Events').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response
+        this.eventosFiltrados = this.eventos
+      },
       error => console.log(error)
-    );
+    ); 
   }
 }
