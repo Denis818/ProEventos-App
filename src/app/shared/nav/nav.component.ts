52adf669d5d8b092622b44e3824854cb0e1ec86b
Denis Developer <denispereiradosreis@gmail.com>
2023-07-01 21:11:42 -0300

    add dark-mode

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Renderer2, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,13 +9,37 @@ import { Router } from '@angular/router';
 })
 export class NavComponent implements OnInit {
   isCollapsed = true;
+  darkMode = false;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router, 
+    private renderer: Renderer2, @Inject(DOCUMENT) 
+    private document: Document) {}
 
   ngOnInit() {
+    this.darkMode = localStorage.getItem('DARK_MODE') === 'true' ? true : false;
+    this.setDarkMode(this.darkMode);
   }
 
   public showMenu(): boolean {
     return this.router.url != '/user/login';
   }
+
+  setDarkMode(isDarkMode: boolean) {
+    this.darkMode = isDarkMode;
+    localStorage.setItem('DARK_MODE', isDarkMode.toString());
+    const themeLink = this.document.getElementById('theme-link') as HTMLLinkElement;
+    if (isDarkMode) {
+      themeLink.href = '../../../assets/themes/bootstrap-darkly.min.css';
+      this.renderer.addClass(this.document.body, 'dark');
+      this.renderer.removeClass(this.document.body, 'light');
+
+    } else { 
+      themeLink.href = '../../../assets/themes/bootstrap-cosmo.min.css';
+      this.renderer.removeClass(this.document.body, 'dark');
+      this.renderer.addClass(this.document.body, 'light');
+    }
+  }
+  
+  
 }
