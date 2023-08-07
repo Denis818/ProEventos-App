5a5e7525404a035810532027854999543d6841a8
Denis Developer <denispereiradosreis@gmail.com>
2023-08-07 19:31:59 -0300

    correções

--- src/app/services/Base/base.service.ts
+++ src/app/services/Base/base.service.ts
@@ -9,19 +9,15 @@ export abstract class BaseService {
   constructor(private http: HttpClient) { }
 
   protected SendHttpRequest(metodo: string, url: string, dados: any = null, contentType: any = null): Observable<any> {
-    const headers = new HttpHeaders({
-      'Content-Type': contentType ?? 'application/json',
-    });
-
     let result = this.http.request(metodo, url, {
       body: dados,
-      headers: headers
+      headers: contentType ?? 'application/json'
     })
       .pipe(map((response: any) => response?.dados),
 
         catchError((error: any) => {
           console.error('Ocorreu um erro:', error);
-          this.ApiErrorMessage(error);
+          this.ApiErrorMessage(error.error.mensagens);
 
           return throwError(() => error);
         })
@@ -30,10 +26,10 @@ export abstract class BaseService {
     return result;
   }
 
-  private ApiErrorMessage(error: any): void {
-      error.error.Mensagens?.forEach((mensagem: any) => {
-        if (mensagem.Tipo !== 200) {
-          console.error('API error: ' + mensagem.Descricao);
+  private ApiErrorMessage(mensagens: any): void {
+      mensagens.forEach((mensagem: any) => {
+        if (mensagem.tipo == 400) {
+          console.error('API error: ' + mensagem.descricao);
         }
       });  
   }
