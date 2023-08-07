d216c83481e327c8021d7799483558a31fcddccf
Denis Developer <denispereiradosreis@gmail.com>
2023-08-07 19:37:26 -0300

    refactor

--- src/app/services/Base/base.service.ts
+++ src/app/services/Base/base.service.ts
@@ -9,28 +9,28 @@ export abstract class BaseService {
   constructor(private http: HttpClient) { }
 
   protected SendHttpRequest(metodo: string, url: string, dados: any = null, contentType: any = null): Observable<any> {
-    let result = this.http.request(metodo, url, {
+    let request = this.http.request(metodo, url, {
       body: dados,
       headers: contentType ?? 'application/json'
-    })
-      .pipe(map((response: any) => response?.dados),
+    });
 
-        catchError((error: any) => {
-          console.error('Ocorreu um erro:', error);
-          this.ApiErrorMessage(error.error.mensagens);
+    const response = request.pipe(map((response: any) => response?.dados),
+      catchError((error: any) => {
+        console.error('Ocorreu um erro:', error);
+        this.ApiErrorMessage(error.error.mensagens);
 
-          return throwError(() => error);
-        })
-      );
+        return throwError(() => error);
+      })
+    );
 
-    return result;
+    return response;
   }
 
   private ApiErrorMessage(mensagens: any): void {
-      mensagens.forEach((mensagem: any) => {
-        if (mensagem.tipo == 400) {
-          console.error('API error: ' + mensagem.descricao);
-        }
-      });  
+    mensagens.forEach((mensagem: any) => {
+      if (mensagem.tipo == 400) {
+        console.error('API error: ' + mensagem.descricao);
+      }
+    });
   }
 }
